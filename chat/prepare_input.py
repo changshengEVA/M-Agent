60415bed3ba5b21ba4a77c69732231e4e52ab7f2
changshengEVA <changshengEVA@gmail.com>
2025-10-21 22:29:18 +0800

    增加roleprompt以及使用方式

--- chat/prepare_input.py
+++ chat/prepare_input.py
@@ -8,7 +8,8 @@ from llama_index.core.indices.base import BaseIndex
 # 0922: 初始化prompt与语言信息
 with open("./prompt.yaml","r",encoding="utf-8") as file:
     prompt_data = yaml.safe_load(file)
-SELFWOM,SELFWOM_WO_OB,OBSERVATION,HISTORY,MEMORY,FIND  =   prompt_data["obserchatprompt"]["selfwom"],\
+ROLE,SELFWOM,SELFWOM_WO_OB,OBSERVATION,HISTORY,MEMORY,FIND  =   prompt_data["roleprompt"],\
+                                prompt_data["obserchatprompt"]["selfwom"],\
                                 prompt_data["obserchatprompt"]["selfwom_wo_ob"],\
                                 prompt_data["obserchatprompt"]["observation"],\
                                 prompt_data["obserchatprompt"]["history"],\
@@ -26,13 +27,13 @@ def find_memory(index: BaseIndex, find: str):
 # 0922: 产生正常对话场景的非记忆性对话
 def emerge_chat_prompt_wo_memory(history,observation = None): 
     if observation:
-        return f"{SELFWOM.format(LANGUAGE)}{OBSERVATION.format(observation)}{HISTORY.format(history)}"
+        return f"{ROLE}{SELFWOM.format(LANGUAGE)}{OBSERVATION.format(observation)}{HISTORY.format(history)}"
     else:
-        return f"{SELFWOM_WO_OB.format(LANGUAGE)}{HISTORY.format(history)}"
+        return f"{ROLE}{SELFWOM_WO_OB.format(LANGUAGE)}{HISTORY.format(history)}"
     
 # 0922：产生正常对话场景的记忆性对话
 def emerge_chat_prompt_w_memory(history,memory,observation = None): 
     if observation: 
-        return f"{SELFWOM.format(LANGUAGE)}{MEMORY.format(memory)}{OBSERVATION.format(observation)}{HISTORY.format(history)}"
+        return f"{ROLE}{SELFWOM.format(LANGUAGE)}{MEMORY.format(memory)}{OBSERVATION.format(observation)}{HISTORY.format(history)}"
     else: 
-        return f"{SELFWOM_WO_OB.format(LANGUAGE)}{MEMORY.format(memory)}{HISTORY.format(history)}"
+        return f"{ROLE}{SELFWOM_WO_OB.format(LANGUAGE)}{MEMORY.format(memory)}{HISTORY.format(history)}"
--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 import os
 from dotenv import load_dotenv
+import json
 load_dotenv()
 from llama_index.core.llms import ChatMessage
+from flipflop.utils import print_with_color
 import argparse
 parser = argparse.ArgumentParser(description='处理方法选择。')
 parser.add_argument('--method', type=str, choices=['local', 'azure', 'openai'], required=True, help='选择处理方法,参照README。')
-parser.add_argument('--memory', type=str, default = "False", help='是否使用记忆')
-parser.add_argument('--store', type=str, default = "False", help='是否使用保存记忆')
+parser.add_argument('--memory', action='store_true', help='是否使用记忆')
+parser.add_argument('--store', action='store_true', help='是否使用保存记忆')
 parser.add_argument('--function', type=str, default = "Talker", choices=["Talker","Email"],help='the function you want to use')
 parser.add_argument('--llm_model_path', type=str, default = "./checkpoints/chatglm3-6b", help='LLM模型权重文件路径')
 parser.add_argument('--embed_model_path', type=str, default = "./checkpoints/bge-large-en-v1.5", help='Embed模型权重文件路径')
 args = parser.parse_args()
-args.memory = (args.memory).strip().upper() == "TRUE"
-args.store = (args.store).strip().upper() == "TRUE"
 
 ##test
 args.llm_model_path = "./checkpoints/chatglm3-6b"
 args.embed_model_path = "./checkpoints/bge-large-en-v1.5"
 
 if not os.path.exists("./data/memory"):
+    print("未检测到记忆文件，正在创建初始记忆文件...")
     os.makedirs("./data/memory")
+
+if not os.path.exists("./data/memory/dialog_history.json"):
+    with open('./data/memory/dialog_history.json','w',encoding = 'utf-8') as f:
+        json.dump([],f,ensure_ascii=False,indent=4)
 
 if args.method == 'local':
     """
@@ -57,7 +62,11 @@ def pre_load():
     index.storage_context.persist(persist_dir="./data/memory/index")
 def main():
     if args.memory:
-        pre_load()
+        try:
+            pre_load()
+        except:
+            print_with_color("记忆加载失败，请检查文件格式是否为最新版本，将使用无记忆模式", "yellow")
+            args.memory = False
     if args.function == "Email":
         from multiff import start_server
         start_server(args)
